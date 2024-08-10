a4efb4fe87fdd5e8cc1d048a74e3cb7e16606cc8
Juan <juanp1310@outlook.com>
2024-08-10 16:24:38 -0300

    feat: rotas da api

---
+++ pages/api/list/[id]/index.js
@@ -0,0 +1,26 @@
+import { deleteTask, getTaskById, updateTask } from "@/controllers/task_controllers";
+
+export default async function Handler(req, res) {
+  const { method, body, query } = req;
+
+  if(method === "GET"){
+    const task = await getTaskById(query.id)
+    return res.status(200).json({
+      data: task
+    })
+  }
+  if(method === "PUT"){
+    const message = await updateTask(body,query.id)
+    return res.status(200).json({
+      message: message
+    })
+  }
+  if(method === "DELETE"){
+    const message = await deleteTask(query.id)
+    if(message) {
+      return res.status(400).json({message: message})
+    }
+    return res.status(204).send()
+  }
+  
+}
---
+++ pages/api/list/index.js
@@ -0,0 +1,30 @@
+import { createTask, getAllTasks } from "@/controllers/task_controllers";
+
+export default async function Handler(req, res) {
+  const { method, body } = req;
+
+  const allowedMethods = ["GET", "POST"];
+
+  if (!allowedMethods.includes(method)) {
+    return res.status(405).json({ error: "method not allowed" });
+  }
+
+  if (method === "GET") {
+    const data = await getAllTasks();
+
+    if (typeof data === "object") {
+      return res.status(200).json({ data: data });
+    }
+
+    return res.status(500).json({ error: data });
+  }
+  
+  if (method === "POST") {
+    const message = await createTask(body)
+    
+    if(message === "Tarefa criada com sucesso"){
+      res.status(201).json({message: message})
+    }
+    res.status(500).json({message: message})
+  }
+}
